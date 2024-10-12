9c8f2f24b37753bf59cbfe3b4afa429effbd0fe0
michellehhy <michellehhygithub@gmail.com>
2024-10-12 22:44:31 +0100

    Add more evaluation and hyypeparameters tuning.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from rdkit import Chem
 from rdkit.Chem import Draw, Descriptors, rdFingerprintGenerator
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+import matplotlib.pyplot as plt
 
 import pandas as pd
 import numpy as np
@@ -86,17 +87,43 @@ class MolecularProcessor:
         y = self.df['LogP']  # Target variable
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        
+        param_dist = {
+        'n_estimators': [100, 200, 300, 500],
+        'max_depth': [None, 10, 20, 30, 50],
+        'min_samples_split': [2, 5, 10],
+        }
+
         model = RandomForestRegressor(n_estimators=100, random_state=42)
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
+        random_search = RandomizedSearchCV(estimator=model, param_distributions=param_dist, n_iter=10, cv=5, scoring='neg_mean_squared_error', n_jobs=-1, random_state=42)
+        random_search.fit(X_train, y_train)
+        print(f"Best parameters: {random_search.best_params_}")
+        print(f"Best Mean Squared Error: {-random_search.best_score_}")
+        best_model = random_search.best_estimator_
+
+        y_pred = best_model .predict(X_test)
+
         mse = mean_squared_error(y_test, y_pred)
         r2 = r2_score(y_test, y_pred)
+        mae = mean_absolute_error(y_test, y_pred)
+        rmse = np.sqrt(mse)
 
-        print(f"Mean Squared Error: {mse}")
-        print(f"R-squared: {r2}")
+        print(f"Test Set - Mean Squared Error: {mse}")
+        print(f"Test Set - R-squared: {r2}")
+        print(f"Mean Absolute Error: {mae}")
+        print(f"Root Mean Squared Error: {rmse}")
+
         test_drugs = self.df.loc[X_test.index, 'DrugName']
         for i in range(len(y_test)):
             print(f"{test_drugs.iloc[i]} - Actual LogP: {y_test.iloc[i]}, Predicted LogP: {y_pred[i]}")
+        
+        plt.scatter(y_test, y_pred)
+        plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], 'r--')
+        plt.xlabel('Actual LogP')
+        plt.ylabel('Predicted LogP')
+        plt.title('Grid Search - Actual vs Predicted LogP')
+        plt.show()
+
 
 if __name__ == '__main__':
     processor = MolecularProcessor('data/diabetes_drugs.csv')
